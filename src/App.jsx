fa02e640f726f3ac694c2711ed42f1d8666872b1
Tran Duc Manh <manhtranduc0202@gmail.com>
2023-12-22 21:16:09 +0700

    set up theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 function App() {
   return (
     <>
       <div>Tranducmanh</div>
       <Button variant="contained">Hello world</Button>
+      <Typography variant="body2" color="text.secondary">
+        Hello
+      </Typography>
     </>
   );
 }
